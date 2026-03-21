4fc30933cd0fed8aa94f028aa98818f1aee74c88
Samuel Lazarin <samuel.lazarin12@gmail.com>
2026-03-21 12:25:41 -0300

    add AboutPage and footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,8 @@
-import AboutUs from "./screens/AboutUs/AboutUs";
-
 function App() {
   return (
     <>
       <h1>Iniciando o Projeto</h1>
       <p>testes</p>
-
-      <AboutUs />
     </>
   );
 }
